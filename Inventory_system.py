feb9c3759bb280d16c13f56bcf3317b1fa768c1b
Mayank3124 <168295729+Mayank3124@users.noreply.github.com>
2024-04-28 19:15:00 +0530

    Inventory_system.py

--- Inventory_system.py
+++ Inventory_system.py
@@ -1,29 +1,36 @@
+inventory = {}
 
-inventory={}
 while True:
-    action= input("enter what want you to do? add, remove, display,quit: ")
-    if action=='add':
-        item=input("enter item name: ")
-        quantity=int(input("enter your quantity: "))
+    action = input("What would you like to do? (add, remove, display, quit): ")
+
+    if action == 'add':
+        item = input("Enter the item name: ")
+        quantity = int(input("Enter the quantity: "))
         if item in inventory:
-            inventory[item]+=quantity
+            inventory[item] += quantity
         else:
-            inventory[item]=quantity
-    elif action=='remove':
-        item=input("enter the name of item you want to remove: ")
-        quantity=int(input("enter the quantity you want to remove: "))
-        if item in inventory and inventory[item]>=quantity:
-            inventory[item]-=quantity
-        elif item in inventory and inventory[item]<quantity:
-            print(f"There are only {inventory[item]} left in {item} left in inventory.")
+            inventory[item] = quantity
+    
+    elif action == 'remove':
+        item = input("Enter the name of the item you want to remove: ")
+        quantity = int(input("Enter the quantity you want to remove: "))
+        if item in inventory and inventory[item] >= quantity:
+            inventory[item] -= quantity
+        elif item in inventory and inventory[item] < quantity:
+            print(f"There are only {inventory[item]} left of {item} in inventory.")
         else:
-            print("ther is no item left in inventory.")
+            print("There is no such item in inventory.")
     
-    elif action=='display':
-        print("Inventory")
-        for key,value in inventory.items():
-            print(f"{key}:{value}")
+    elif action == 'display':
+        print("Inventory:")
+        for key, value in inventory.items():
+            print(f"{key}: {value}")
     
+    elif action == 'quit':
+        break
+    
+    else:
+        print("Invalid entry. Please try again.")
     elif action =='quit':
         break
     else:
